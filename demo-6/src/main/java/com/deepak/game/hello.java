fe0800002fb197306d349827a89c1f1c0ba234c6
Mani Deepak <mattaparthisaimanideepak@gmail.com>
2024-06-03 10:18:49 +0530

    second commit

---
+++ demo-6/src/main/java/com/deepak/game/hello.java
@@ -0,0 +1,16 @@
+package com.deepak.game;
+
+public class hello {
+	public void up() {
+    	System.out.println("HELLO UP");
+    }
+    public void down() {
+    	System.out.println("HELLO DOWN");
+    }
+    public void left() {
+    	System.out.println("HELLO LEFT");
+    }
+    public void right() {
+    	System.out.println("HELLO RIGHT");
+    }
+}
